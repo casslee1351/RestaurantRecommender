53cc8fea18059b50c637b163744e2a3a0e9c8a82
casslee1351 <casslee1351@gmail.com>
2022-10-23 16:09:02 -0500

    taking out irrelevant imports

--- rating/spoonme-project/restaurants/views.py
+++ rating/spoonme-project/restaurants/views.py
@@ -1,5 +1,5 @@
 import re
-from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect, reverse
+from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect
 from django.http import JsonResponse
 from django.contrib import messages
 from django.contrib.auth.models import User
